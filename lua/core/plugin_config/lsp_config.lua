7e7f19ff34e8eb379b7b5c24ae736c34e3ea8fa3
drone <drone@ubuntu.localdomain>
2024-01-08 14:10:11 +0300

    add intelephense

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,5 +1,5 @@
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "solargraph", "tsserver" }
+  ensure_installed = { "lua_ls", "solargraph", "tsserver", "intelephense" }
 })
 
 local lspconfig = require('lspconfig')
@@ -32,6 +32,7 @@ require("lspconfig").solargraph.setup({})
 require("lspconfig").tsserver.setup({})
 require("lspconfig").gopls.setup({})
 require("lspconfig").tailwindcss.setup({})
+require("lspconfig").intelephense.setup({})
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
